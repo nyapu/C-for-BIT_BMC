2740036264debd4a2cd130045aded5309bc5a30a
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-12-29 11:49:28 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main() {
+  std::cout << "Hello MUltiple Campus!\n";
+
+
+  cout<<"hello cyber";
+}
+char* mySecret = getenv("123454321")
